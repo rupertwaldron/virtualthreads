a8b439a6f056756fcf0e2fc988cc862000d08f47
rupertwaldron <rupert.waldron@yahoo.co.uk>
2023-05-13 20:03:53 +0100

    Initial commit

---
+++ src/main/java/com/ruppyrup/virtualdemo/VirtualdemoApplication.java
@@ -0,0 +1,13 @@
+package com.ruppyrup.virtualdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualdemoApplication.class, args);
+    }
+
+}
